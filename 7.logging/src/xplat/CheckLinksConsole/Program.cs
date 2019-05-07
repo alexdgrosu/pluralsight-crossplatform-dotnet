ddbb0d7f868d74a1d5bd99fb66f76ed10d47f5aa
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2019-05-07 19:44:07 +0200

    Add 7th module - Logging

---
+++ 7.logging/src/xplat/CheckLinksConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CheckLinksConsole
+{
+    using Microsoft.Extensions.Logging;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = new Config(args);
+            Logs.Init(config.ConfigurationRoot);
+            var logger = Logs.Factory.CreateLogger<Program>();
+
+            Directory.CreateDirectory(config.Output.ReportDirectory);
+            logger.LogInformation(200, $"Saving report to {config.Output.ReportFilePath}");
+
+            var client = new HttpClient();
+            var body = client.GetStringAsync(config.Site);
+            logger.LogDebug(body.Result);
+
+            var links = LinkChecker.GetLinks(config.Site, body.Result);
+
+            var checkedLinks = LinkChecker.CheckLinks(links);
+            using (var file = File.CreateText(config.Output.ReportFilePath))
+            {
+                foreach (var link in checkedLinks.OrderByDescending(l => l.IsMissing))
+                {
+                    var status = link.IsMissing ? "MISSING" : "OK";
+                    file.WriteLine($"{status} - {link.Link}");
+                }
+            }
+        }
+    }
+}
